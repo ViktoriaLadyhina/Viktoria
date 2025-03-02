1d880d7e643f769a9fdb103c0728586f5e028d1d
ViktoriaLadyhina <vika.ladyhina@gmail.com>
2025-03-02 19:23:31 +0100

    Add files via upload

--- HW(Les20 - canvas)/js/script.js
+++ HW(Les20 - canvas)/js/script.js
@@ -1,35 +1,16 @@
 console.log("Homework (Lesson 20)");
 
-// Создать класс Person который получает размеры и отрисовавает красный прямоугольник
-// moveTop
-// moveDown
-// moveLeft
-// moveRight
-// которые на 5 пикселей двигают персонажа в соотвествующей направлении
-// при нажатии на стрелочки двигать прямоугольник
+// я немного усложнила для себя задание - должно быть два класса - треугольник и прямоугольник, которые наследуюься от класса Фигура. В классе Фигура - методы сдвигов (кнопки) и кнопки переключения между фигурами. 
+// Не могу понять почему в кнопки переключения не вставился i-элемент, хотя работают....
+// при создании экземпляра указывается размер и цвет фигур.
 
 const canvasElem = document.querySelector("canvas")
 const ctx = canvasElem.getContext("2d")
 
-
-class Person {
-    constructor(width, height) {
-        this.width = width;
-        this.height = height;
+class Figure {
+    constructor(){
         this.x = 100;
         this.y = 100;
-    }
-
-    creatTriangle() {
-        ctx.beginPath()
-        ctx.fillStyle = "red"
-        ctx.strokeStyle = "red"
-        ctx.moveTo(this.x,                  this.y)
-        ctx.lineTo(this.x,                  this.y + this.height)
-        ctx.lineTo(this.x + this.width,     this.y + this.height)
-        ctx.lineTo(this.x,                  this.y)
-        ctx.stroke()
-        ctx.fill()
     }
 
     moveTop() {
@@ -52,33 +33,93 @@ class Person {
         this.clear();
     }
 
-    clear(){
+    clear() {
         ctx.clearRect(0, 0, canvasElem.width, canvasElem.height);
+    }
+}
+
+class Triangle extends Figure {
+    constructor(width, height, color) {
+        super()
+        this.width = width;
+        this.height = height;
+        this.color = color;
+    }
+
+    creatTriangle() {
+        ctx.beginPath()
+        ctx.fillStyle = this.color
+        ctx.moveTo(this.x, this.y)
+        ctx.lineTo(this.x, this.y + this.height)
+        ctx.lineTo(this.x + this.width, this.y + this.height)
+        ctx.lineTo(this.x, this.y)
+        ctx.stroke()
+        ctx.fill()
+    }
+
+    clear() {
+        super.clear(this.x, this.y)
         this.creatTriangle();
     }
 }
 
+class Rectangle extends Figure {
+    constructor(width, height, color) {
+        super();
+        this.width = width;
+        this.height = height;
+        this.color = color;
+    }
 
-const person = new Person(400, 300);
-person.creatTriangle()
+    creatRectangle() {
+        ctx.fillStyle = this.color
+        ctx.fillRect(this.x, this.y, this.width, this.height)
+    }
+
+    clear() {
+        super.clear(this.x, this.y)
+        this.creatRectangle();
+    }
+}
 
 
+const triangle = new Triangle(400, 300, "red");
+const rectangle = new Rectangle(400, 300, "green");
+
+
+
+// кнопки для переключения фигур
+let curFigure = triangle;
+const switchingTriangle = document.querySelector(".triangle")
+switchingTriangle.addEventListener("click", function(){
+    curFigure = triangle;
+    curFigure.clear();
+})
+
+const switchingRectangle = document.querySelector(".rectangle")
+switchingRectangle.addEventListener("click", function(){
+    curFigure = rectangle;
+    curFigure.clear();
+})
+
+
+// кнопки для сдвига фигуры
 const upArrow = document.querySelector(".top");
-upArrow.addEventListener("click", function(){
-    person.moveTop();
+upArrow.addEventListener("click", function () {
+    curFigure.moveTop();
 })
 
 const downArrow = document.querySelector(".down");
-downArrow.addEventListener("click", function(){
-    person.moveDown();
+downArrow.addEventListener("click", function () {
+    curFigure.moveDown();
 })
 
 const leftArrow = document.querySelector(".left");
-leftArrow.addEventListener("click", function(){
-    person.moveLeft();
+leftArrow.addEventListener("click", function () {
+    curFigure.moveLeft();
 })
 
 const rightArrow = document.querySelector(".right");
-rightArrow.addEventListener("click", function(){
-    person.moveRight();
+rightArrow.addEventListener("click", function () {
+    curFigure.moveRight();
 })
